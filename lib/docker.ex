8d6e4b442a72977286c774733f6380ec69c344d8
Manuel Zubieta <mazubieta@gmail.com>
2018-11-13 16:40:10 -0600

    fix address used with unix socket

--- lib/docker.ex
+++ lib/docker.ex
@@ -24,9 +24,8 @@ defmodule Docker do
   ```
   """
   def config(addr \\ "unix:///var/run/docker.sock") do
-      Map.put(%Docker{},:addr, addr)
+    Map.put(%Docker{}, :addr, addr)
   end
-
 
   @doc ~S"""
   获取容器列表.
@@ -37,7 +36,7 @@ defmodule Docker do
     iex > Docker.containers(config)
   ```
   """
-  def containers(docker), do: docker.req.("/containers/json?all=true",docker.addr)
+  def containers(docker), do: docker.req.("/containers/json?all=true", docker.addr)
 
   @doc ~S"""
   获取镜像列表.
@@ -48,7 +47,7 @@ defmodule Docker do
     iex > Docker.images(config)
   ```
   """
-  def images(docker), do: docker.req.("/images/json",docker.addr)
+  def images(docker), do: docker.req.("/images/json", docker.addr)
 
   @doc ~S"""
   获取 Docker Info.
@@ -59,7 +58,7 @@ defmodule Docker do
     iex > Docker.info(config)
   ```
   """
-  def info(docker), do: docker.req.("/info",docker.addr)
+  def info(docker), do: docker.req.("/info", docker.addr)
 
   @doc ~S"""
   获取 Docker Swarm nodes Info.
@@ -70,7 +69,7 @@ defmodule Docker do
     iex > Docker.nodes(config)
   ```
   """
-  def nodes(docker), do: docker.req.("/nodes",docker.addr)
+  def nodes(docker), do: docker.req.("/nodes", docker.addr)
 
   @doc ~S"""
   获取 Docker 版本信息.
@@ -81,12 +80,12 @@ defmodule Docker do
     iex > Docker.info(config)
   ```
   """
-  def version(docker) ,do: docker.req.("/version",docker.addr)
+  def version(docker), do: docker.req.("/version", docker.addr)
 
   @doc """
   获取 Volumes 列表
   """
-  def volumes(docker) ,do: docker.req.("/volumes",docker.addr)
+  def volumes(docker), do: docker.req.("/volumes", docker.addr)
 
   @doc ~S"""
   添加 Docker Event 监听器.
@@ -105,8 +104,8 @@ defmodule Docker do
   Docker.add_event_listener(config,spawn(Example, :listen, []))
   ```
   """
-  def add_event_listener(docker,pid \\self) do
-    Docker.Request.get("/events",docker.addr,pid)
+  def add_event_listener(docker, pid \\ self) do
+    Docker.Request.get("/events", docker.addr, pid)
   end
 
   @doc ~S"""
@@ -118,8 +117,8 @@ defmodule Docker do
   Docker.add_event_listener(config,container_id)
   ```
   """
-  def add_log_listener(docker,id,pid\\self) do
-    Docker.Request.get("/containers/#{id}/logs",docker.addr,pid)
+  def add_log_listener(docker, id, pid \\ self) do
+    Docker.Request.get("/containers/#{id}/logs", docker.addr, pid)
   end
 
   @doc ~S"""
@@ -133,9 +132,9 @@ defmodule Docker do
   ```
   具体参数参考 (Docker Remote API)[https://docs.docker.com/engine/reference/api/docker_remote_api_v1.24/#/create-a-container]
   """
-  def create_container(docker,data) do
+  def create_container(docker, data) do
     data_string = Poison.encode!(data)
-    Docker.Request.post("/containers/create",docker.addr,data_string)
+    Docker.Request.post("/containers/create", docker.addr, data_string)
   end
 
   @doc ~S"""
@@ -146,9 +145,10 @@ defmodule Docker do
   config = Docker.config(address)
   {:ok,resp} = Docker.start(config,"containerId")
   """
-  def start(docker,id) do
-    Docker.Request.post("/containers/#{id}/start",docker.addr)
+  def start(docker, id) do
+    Docker.Request.post("/containers/#{id}/start", docker.addr)
   end
+
   @doc """
   CN:停止容器
   EN:Stop a container¶
@@ -157,42 +157,43 @@ defmodule Docker do
   config = Docker.config(address)
   {:ok,resp} = Docker.stop(config,"containerId")
   """
-  def stop(docker,id) do
-     Docker.Request.post("/containers/#{id}/stop",docker.addr)
+  def stop(docker, id) do
+    Docker.Request.post("/containers/#{id}/stop", docker.addr)
   end
 
   @doc ~S"""
   CN:获取指定容器ID的信息
   EN:Inspect a container
   """
-  def container(docker,id) do
-   docker.req.("/containers/#{id}/json",docker.addr)
+  def container(docker, id) do
+    docker.req.("/containers/#{id}/json", docker.addr)
   end
 
-  def remove_container(docker,id) do
-     Docker.Request.delete("/containers/#{id}",docker.addr)
+  def remove_container(docker, id) do
+    Docker.Request.delete("/containers/#{id}", docker.addr)
   end
 
   @doc ~S"""
   CN:列出容器中所有的进程信息
   EN:List processes running inside a container
   """
-  def top(docker,id), do: docker.req.("/containers/#{id}/top",docker.addr)
+  def top(docker, id), do: docker.req.("/containers/#{id}/top", docker.addr)
 
   @doc ~S"""
   CN:列出容器中的资源状态
   EN:Get container stats based on resource usage
   """
-  def stats(docker,id), do: docker.req.("/containers/#{id}/stats",docker.addr)
+  def stats(docker, id), do: docker.req.("/containers/#{id}/stats", docker.addr)
 
   @doc ~S"""
   CN:列出容器文件系统的变更
   EN:Inspect changes on a container’s filesystem
   """
-  def changes(docker,id), do: docker.req.("/containers/#{id}/changes",docker.addr)
+  def changes(docker, id), do: docker.req.("/containers/#{id}/changes", docker.addr)
+
   @doc ~S"""
   CN:强制关闭容器
   EN:Kill a container
   """
-  def kill(docker,id), do: docker.req.("/containers/#{id}/kill",docker.addr)
+  def kill(docker, id), do: docker.req.("/containers/#{id}/kill", docker.addr)
 end
--- lib/docker/request.ex
+++ lib/docker/request.ex
@@ -9,44 +9,45 @@ defmodule Docker.Request do
   defp init(url) do
     # 因为 HTTP Protocol 的关系用 line 来recv 比较舒服
     case url do
-        "unix://"<>path ->
-          opts = [:binary,packet: :line,active: false, keepalive: true]
-          :gen_tcp.connect({:local,path}, 0, opts)
-        "http://"<>_ ->
-          uri = URI.parse(url)
-          opts = [:binary,packet: :line,active: false]
-          :gen_tcp.connect(to_charlist(uri.host),uri.port,opts)
-      end
+      "unix://" <> path ->
+        opts = [:binary, packet: :line, active: false, keepalive: true]
+        :gen_tcp.connect({:local, path}, 0, opts)
+
+      "http://" <> _ ->
+        uri = URI.parse(url)
+        opts = [:binary, packet: :line, active: false]
+        :gen_tcp.connect(to_charlist(uri.host), uri.port, opts)
+    end
   end
 
-
-  defp send_request(socket,url,method \\ "GET",data\\nil) do
+  defp send_request(socket, url, method \\ "GET", data \\ nil) do
     # 发送数据
     host =
       case url do
-        "unix://"<>_ -> "var.run.docker"
-        "http://"<>_ -> URI.parse(url).host
+        "unix://" <> _ -> "var.run.docker"
+        "http://" <> _ -> URI.parse(url).host
       end
 
     data_string = "#{method} #{url || "/"} HTTP/1.1\r\nHost: #{host}\r\n"
+
     data_string =
       if data do
-        data_string <>"Content-Type: application/json\r\n"
-                    <>"Connection: Keep-Alive\r\n"
-                    <>"Content-Length: #{String.length(data)}\r\n\r\n"
-                    <>"#{data}"
+        data_string <>
+          "Content-Type: application/json\r\n" <>
+          "Connection: Keep-Alive\r\n" <>
+          "Content-Length: #{String.length(data)}\r\n\r\n" <> "#{data}"
       else
-        data_string<>"\r\n"
+        data_string <> "\r\n"
       end
-    Logger.debug data_string
-    :ok = :gen_tcp.send(socket,data_string)
-  end
 
+    Logger.debug(data_string)
+    :ok = :gen_tcp.send(socket, data_string)
+  end
 
   defp parse_response(socket) do
     # 解析 Socket 收到的数据
-    resp = Response.handle_header(socket,%Response{})
-    resp =  if resp.len > 0 or resp.chunked ,do: Response.handle_body(socket,resp,0), else: resp
+    resp = Response.handle_header(socket, %Response{})
+    resp = if resp.len > 0 or resp.chunked, do: Response.handle_body(socket, resp, 0), else: resp
     # HTTP 中 Socket 不复用,需要关闭
     :gen_tcp.close(socket)
     resp.body
@@ -55,29 +56,32 @@ defmodule Docker.Request do
   @doc """
   非 Keep-Alive 的请求
   """
-  def get(url,addr) do
-    target_url = URI.merge(URI.parse(addr), url)|> to_string
-    {:ok,socket} = init(target_url)
-    send_request(socket,target_url)
+  def get(url, addr) do
+    {:ok, socket} = init(addr)
+    addr = "http://docker/"
+
+    Logger.debug("url: #{inspect(url)}")
+    Logger.debug("addr: #{inspect(addr)}")
+
+    target_url = URI.merge(URI.parse(addr), url) |> to_string
+    send_request(socket, target_url)
     parse_response(socket)
   end
 
-
-  def delete(url,addr) do
-    target_url = URI.merge(URI.parse(addr), url)|> to_string
-    {:ok,socket} = init(target_url)
-    send_request(socket,target_url,"DELETE")
+  def delete(url, addr) do
+    target_url = URI.merge(URI.parse(addr), url) |> to_string
+    {:ok, socket} = init(target_url)
+    send_request(socket, target_url, "DELETE")
     parse_response(socket)
   end
-
 
   @doc """
   POST 请求
   """
-  def post(url,addr,data\\nil) do
-    target_url = URI.merge(URI.parse(addr), url)|> to_string
-    {:ok,socket} = init(target_url)
-    send_request(socket,target_url,"POST",data)
+  def post(url, addr, data \\ nil) do
+    target_url = URI.merge(URI.parse(addr), url) |> to_string
+    {:ok, socket} = init(target_url)
+    send_request(socket, target_url, "POST", data)
     parse_response(socket)
   end
 
@@ -86,13 +90,14 @@ defmodule Docker.Request do
 
   *注意* 流中断之后，要客户端重新连接，中断期间的数据不会重发
   """
-  def get(url,addr,pid) do
-    target_url = URI.merge(URI.parse(addr), url)|> to_string
-    {:ok,socket} = init(target_url)
-    send_request(socket,target_url)
+  def get(url, addr, pid) do
+    target_url = URI.merge(URI.parse(addr), url) |> to_string
+    {:ok, socket} = init(target_url)
+    send_request(socket, target_url)
+
     Task.start_link(fn ->
-      resp = Response.handle_header(socket,%Response{})
-      resp = Response.handle_body(socket,resp,0,pid)
+      resp = Response.handle_header(socket, %Response{})
+      resp = Response.handle_body(socket, resp, 0, pid)
       # HTTP 中 Socket 不复用,需要关闭
       :gen_tcp.close(socket)
       {:ok, resp}
